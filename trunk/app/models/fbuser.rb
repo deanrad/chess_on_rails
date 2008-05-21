2ee71598c5b4605de215156ca3df3a185d295499
Dean Radcliffe <chicagogrooves@gmail.com>
2008-05-21 04:25:59 +0000

    new tweak for user installation

--- trunk/app/models/fbuser.rb
+++ trunk/app/models/fbuser.rb
@@ -5,5 +5,8 @@ class Fbuser < ActiveRecord::Base
 	def self.install( fb_user_id )
 		p = Player.create( :name => "Facebook #{fb_user_id}" )
 		Fbuser.create( :facebook_user_id => fb_user_id, :playing_as => p ) #returns what was created
+
+		# give them a game with me 
+		m = Match.create( :player1 => p, :player2 => Player.find(1) )
 	end
 end
