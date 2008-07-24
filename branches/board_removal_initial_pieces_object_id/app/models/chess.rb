d0090e6e1db219072d3cdffdd175d68fc9237073
Dean Radcliffe <chicagogrooves@gmail.com>
2008-07-24 15:57:08 +0000

    Corrected flaw with chess.initial_pieces internal caching - got it actually working

--- branches/board_removal_initial_pieces_object_id/app/models/chess.rb
+++ branches/board_removal_initial_pieces_object_id/app/models/chess.rb
@@ -2,36 +2,37 @@ class Chess
 
   Files = "abcdefgh"
   Ranks = "12345678"
-  
-  def self.valid_position?(pos)
-    return false unless pos and pos.length == 2
-    return false unless Files.include? pos[0] and Ranks.include? pos[1]
+
+  @@pieces = []
     
-    return true
-  end
+   def self.valid_position?(pos)
+      return false unless pos and pos.length == 2
+      return false unless Files.include? pos[0] and Ranks.include? pos[1]
+      return true
+   end
 
-  def self.initial_pieces
-    
-    @@pieces = []
-    
-    [ [:white, '1', '2'], [:black, '8', '7'] ].each do |side, back_rank, front_rank|
-
-      ('a'..'h').each do |file|
-        @@pieces << Piece.new( side, "#{file}_pawn", file + front_rank )
-      end
-
-      @@pieces << Piece.new( side, :queens_rook, 'a'+back_rank )
-      @@pieces << Piece.new( side, :queens_knight, 'b'+back_rank )
-      @@pieces << Piece.new( side, :queens_bishop, 'c'+back_rank )
-      @@pieces << Piece.new( side, :queen, 'd'+back_rank )
-      @@pieces << Piece.new( side, :king, 'e'+back_rank )
-      @@pieces << Piece.new( side, :kings_bishop, 'f'+back_rank )
-      @@pieces << Piece.new( side, :kings_knight, 'g'+back_rank )
-      @@pieces << Piece.new( side, :kings_rook, 'h'+back_rank )
-
-    end
-
-    return @@pieces
-  end
+   def self.initial_pieces
+      return @@pieces if @@pieces.length > 0
+   
+      #for each color (with their corresponding front and back ranks)    
+      [ [:white, '1', '2'], [:black, '8', '7'] ].each do |side, back_rank, front_rank|
+         #put a pawn on each file of the front rank
+         ('a'..'h').each do |file|
+            @@pieces << Piece.new( side, "#{file}_pawn", file + front_rank )
+         end
+         
+         #place the rest or the pieces along the back rank
+         @@pieces << Piece.new( side, :queens_rook, 'a'+back_rank )
+         @@pieces << Piece.new( side, :queens_knight, 'b'+back_rank )
+         @@pieces << Piece.new( side, :queens_bishop, 'c'+back_rank )
+         @@pieces << Piece.new( side, :queen, 'd'+back_rank )
+         @@pieces << Piece.new( side, :king, 'e'+back_rank )
+         @@pieces << Piece.new( side, :kings_bishop, 'f'+back_rank )
+         @@pieces << Piece.new( side, :kings_knight, 'g'+back_rank )
+         @@pieces << Piece.new( side, :kings_rook, 'h'+back_rank )
+      end 
+      
+      @@pieces
+   end
   
 end
