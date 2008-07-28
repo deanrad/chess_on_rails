53f0c1bfd60a4ce0fb1c357b257efe670367dd1e
Dean Radcliffe <chicagogrooves@gmail.com>
2008-07-28 17:49:35 +0000

    additional test

--- trunk/test/functional/match_controller_test.rb
+++ trunk/test/functional/match_controller_test.rb
@@ -8,27 +8,27 @@ class MatchControllerTest < ActionController::TestCase
   end 
 
   def test_white_view_of_board_when_requested_by_white
-      get :show, { :id => matches(:paul_vs_dean).id }, {:player_id => players(:paul).id }
-      assert_response 200
-      assert_equal :white, assigns['viewed_from_side']
+    get :show, { :id => matches(:paul_vs_dean).id }, {:player_id => players(:paul).id }
+    assert_response 200
+    assert_equal :white, assigns['viewed_from_side']
 
   #assert 8 is first in the list so when board rendered down page, 1 is at the bottom
-      assert_equal '8', assigns['ranks'][0].chr 
+    assert_equal '8', assigns['ranks'][0].chr 
   end
 
   def test_black_view_of_board_when_requested_by_black
-      get :show, { :id => matches(:paul_vs_dean).id }, {:player_id => players(:dean).id }
+    get :show, { :id => matches(:paul_vs_dean).id }, {:player_id => players(:dean).id }
 
-      assert_response 200
-      assert_equal :black, assigns['viewed_from_side']
+    assert_response 200
+    assert_equal :black, assigns['viewed_from_side']
 
-      assert_equal '1', assigns['ranks'][0].chr 
+    assert_equal '1', assigns['ranks'][0].chr 
   end
 
   def test_requesting_ended_match_yields_result_template
-  get :show, { :id => matches(:resigned).id }, {:player_id => players(:dean).id }
-      assert_response 200
-      assert_template 'match/result'
+    get :show, { :id => matches(:resigned).id }, {:player_id => players(:dean).id }
+    assert_response 200
+    assert_template 'match/result'
   end
 
   def test_gets_form_for_creating_new_match
@@ -38,7 +38,7 @@ class MatchControllerTest < ActionController::TestCase
   end
 
   def test_creates_new_match_opponent_as_black
-      post :create, {:opponent_side => 2, :opponent_id => players(:paul).id}, {:player_id => players(:dean).id }
+    post :create, {:opponent_side => 2, :opponent_id => players(:paul).id}, {:player_id => players(:dean).id }
     assert_not_nil assigns['match']
     assert_response 302
   end
@@ -52,6 +52,12 @@ class MatchControllerTest < ActionController::TestCase
     get :index, nil, {:player_id => players(:paul).id }
     assert_equal [], assigns['matches'].find { |m| m.id == matches(:resigned).id }
     assert_response 200
+  end
+
+  def test_verifies_showing_a_match_renders_the_board
+    get :show, { :id => 9 }, {:player_id => players(:dean).id }
+    assert_response 200
+    assert_select 'div#board_table'
   end
 
 end
