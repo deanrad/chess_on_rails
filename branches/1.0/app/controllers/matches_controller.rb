3db63313812d2e480f7e5b2727c6184945646708
Dean Radcliffe <chicagogrooves@gmail.com>
2008-07-30 19:16:42 +0000

    some views and partials, nicely laid out helpers that don't clutter models or controllers, very clean

--- branches/1.0/app/controllers/matches_controller.rb
+++ branches/1.0/app/controllers/matches_controller.rb
@@ -1,3 +1,14 @@
 # Deals with the creation and updating of matches, such as creation, resignation
 class MatchesController < ApplicationController
+
+  # GET /matches/N
+  def show
+    @match = Match.find( params[:id] )
+  end
+
+  # GET /matches
+  def index
+    @matches = current_player.matches if current_player
+  end
+      
 end
--- branches/1.0/app/helpers/matches_helper.rb
+++ branches/1.0/app/helpers/matches_helper.rb
@@ -1,2 +1,41 @@
+#Contains functions for laying out the chess board
 module MatchesHelper
+  
+  #iterates over the rows and yields columns in the correct order for rendering the board for white or black
+  def positions_by_row_as( side )
+    
+    rows_in_order = []
+    if side==:white
+      0.upto(7){ |i| rows_in_order << i }
+    else
+      7.downto(0){ |i| rows_in_order << i } 
+    end
+    
+    rows_in_order.each do |row_offset|
+        from, to = [row_offset*8, row_offset*8 + 7]
+        row_pieces = Position::POSITIONS[ (from..to) ]
+        yield side==:white ? row_pieces : row_pieces.reverse
+    end
+    
+  end
+  
+  def files_as( side )
+    files = side==:white ? "abcdefgh" : "hgfedcba"
+    files.each_byte{ |f| yield f.chr }
+  end
+  
+  def side_of(match, player)
+    return :white unless match and player
+    match.player1 == player ? :white : :black
+  end
+  
+  #returns the color of a square given a symbol or a string representing a position
+  def square_color( pos )
+    pos = pos.to_s
+    (pos[0]+pos[1]) % 2 == 1 ? :white : :black
+  end
+  
+  def render_piece( piece )
+    return "&nbsp;" unless piece
+  end
 end
