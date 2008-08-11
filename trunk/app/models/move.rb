9bad8924ff2986bd483af366435ea38d36c4e503
Dean Radcliffe <chicagogrooves@gmail.com>
2008-08-11 16:08:01 +0000

    restored coverage

--- trunk/app/models/move.rb
+++ trunk/app/models/move.rb
@@ -64,10 +64,7 @@ class Move < ActiveRecord::Base
     @board.consider_move( self ) do
       #if their move will leave them in check at the end of it we need to void the move and tell them why
       if @board.in_check?( @piece_moving.side )
-        if currently_in_check
-          errors.add_to_base "You are in check and must move out of check"
-        else
-          errors.add_to_base "You can not move your king into check"
+          errors.add_to_base currently_in_check ? "You are in check and must move out of check" : "You can not move your king into check"
         end
       end
     end
