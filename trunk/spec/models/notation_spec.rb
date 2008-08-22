ca6c3b362f55c748e33243e20c2bf1874df0a716
Dean Radcliffe <chicagogrooves@gmail.com>
2008-08-22 06:25:31 +0000

    most of notation functionality

---
+++ trunk/spec/models/notation_spec.rb
@@ -0,0 +1,135 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe 'Notation - ' do
+  before(:all) do
+    #TODO - dry up these initial copies of test objects
+    @initial_board = Board.initial_board    
+
+    @white_king, @white_queen = [ Piece.new(:king, :white), Piece.new(:queen, :white) ]
+    @black_king, @black_queen = [ Piece.new(:king, :black), Piece.new(:queen, :black) ]
+    @kings_and_queens= Board[ :d1 => @white_queen, :e1 => @white_king, :d8 => @black_queen, :e8 => @black_king ]
+
+    @promotable = Board[:d7 => Pawn.new(:white, :d)]
+
+    @initial_knight_move  = Notation.new( 'Nc3', @initial_board )
+    @initial_knight_move_by_coords  = Notation.new( :b1, :c3, @initial_board )
+    @promotable = Board[:d7 => Pawn.new(:white, :d)]
+  end
+
+  describe 'Constructors' do
+    it 'should be creatable given a from and to coordinate pair, and a board' do
+      n  = Notation.new( :b1, :c3, @initial_board )
+      n.board.should == @initial_board
+    end
+    it 'should be creatable given a notation and a board' do
+      n  = Notation.new( 'Nc3', @initial_board )
+      n.board.should == @initial_board
+    end
+  end
+
+  describe 'Functions' do
+    it 'should be able to map piece types to abbreviations' do
+      Notation.abbrev(:knight).should == 'N'
+      Notation.abbrev(:bishop).should == 'B'
+      #etc..
+    end
+    it 'should be able to map abbreviations to piece types' do
+      Notation.role_of('B').should == :bishop
+      Notation.role_of('N').should == :knight
+    end
+  end
+  
+  describe 'Data Fields' do
+    it 'should encode for the role of the piece that is moving' do
+      @initial_knight_move.role = :knight
+      @initial_knight_move.role.should == :knight
+    end
+    it 'should encode for whether a capture occcurred' do
+      @initial_knight_move.capture = true
+      @initial_knight_move.capture.should == true
+    end
+
+    it 'should encode for the destination square' do
+      @initial_knight_move.to_coord = :c4
+      @initial_knight_move.to_coord.should == :c4
+    end
+
+    it 'should encode for which promotion choice was made' do
+      @initial_knight_move.promotion_choice = :queen
+      @initial_knight_move.promotion_choice.should == :queen
+    end
+
+    it 'should reflect whether check occurred on that move' do
+      @initial_knight_move.check = true
+      @initial_knight_move.check.should == true
+    end
+
+    it 'should reflect whether checkmate occurred on that move' do
+      @initial_knight_move.checkmate = true
+      @initial_knight_move.checkmate.should == true
+    end
+
+  end
+  
+  describe 'Serialization' do
+    it 'should store the piece abbreviation in the first character for non-pawns' do
+      @initial_knight_move_by_coords.to_s[0,1].should == 'N'
+    end
+
+    it 'should have an optional x in the next position if the capture field is true' do
+      n = Notation.new(:d1, :d8, @kings_and_queens)
+      ns = n.to_s
+      ns[0,1].should == 'Q'
+      ns[1,1].should == 'x'
+    end
+
+    it 'should have a complete to coordinate present only after previous fields' do
+      pawn_move_notation = Notation.new(:d2, :d4, @initial_board)
+      queen_capture_notation = Notation.new(:d1, :d8, @kings_and_queens)
+
+      #role-omitted pawn move
+      pawn_move_notation.to_s[0,2].should == 'd4'
+      
+      #normal noncapture      
+      @initial_knight_move_by_coords.to_s[1,2].should == 'c3'
+      
+      #normal capture
+      queen_capture_notation.to_s[2,2].should == 'd8'
+    end
+
+    it 'should have = followed by a piece type abbreviation if promotion occurred' do
+      n = Notation.new(:d7, :d8, @promotable)
+      n.to_s.should == 'd8=Q'
+    end
+    
+    it 'should have a + following the to coordinate if check occurred but not checkmate' do
+      queen_capture_notation = Notation.new(:d1, :d8, @kings_and_queens)
+      queen_capture_notation.to_s[4,1].should == '+'
+    end
+    
+    it 'should have a # following the to coordinate if checkmate occurred'
+    # not developed yet
+    # do
+    #  matable_board = Board[:d1 => Rook.new(:white, :kings), :d2 => Queen.new(:white), :d8 => King.new(:black) ]
+    #  n = Notation.new(:d2, :d7, matable_board)
+    #  n.to_s[3,1].should == '#'
+    #end
+  end
+
+  describe 'Piece disambiguation' do
+    it 'should be necessary if more than one of a piece type could have moved to the to coordinate'
+    it 'should preferentially be the file that differentiates between the pieces if it does so'
+    it 'should secondarily be the rank that differentiates between the pieces'
+  end
+  
+  describe 'Coordinates to Notation' do
+    it 'should have an X in the 2nd character position if capturing'
+  end
+
+  describe 'Notation to Coordinates' do
+    it 'should raise an error if more than one of that piece type could move to that destination square'
+    it 'should return a from and to coordinate pair if one is understandable from notation with respect to a board'
+    it 'should raise an Exception if a from and to coordinate pair cannot be found'
+  end   
+  
+end
