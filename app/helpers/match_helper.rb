bc345b772933667bf9d90b3cd4832ea28bcaa30c
Dean Radcliffe <dradcliffe@dradcliffe-laptop.(none)>
2009-01-21 19:42:19 -0600

    Finished applying stcorbett's sweet piece switching machinery to the current leading version of the app

--- app/helpers/match_helper.rb
+++ app/helpers/match_helper.rb
@@ -1,2 +1,21 @@
 module MatchHelper
+
+  # checks for existance of .gif file in the current set's directory
+  # if no .gif, uses .png extension
+  def image_source_of( piece )
+    "/images/sets/default/#{piece.role.to_s}_#{piece.side.to_s[0,1]}.gif"
+    session[:set] ||= 'default'
+    path = "/images/sets/#{session[:set]}/"
+    extension = gif_file_exists?(piece, path) ? ".gif" : ".png"
+    path + piece_file_name(piece) + extension
+   end
+  
+  def piece_file_name(piece)
+    "#{piece.role.to_s}_#{piece.side.to_s[0,1]}"
+  end
+  
+  def gif_file_exists?(piece, path)
+    File.exists?( Rails.public_path + path + piece_file_name(piece) + ".gif" )
+  end
+
 end
