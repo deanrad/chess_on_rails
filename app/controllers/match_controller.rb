b554df90cad92fed2a7dda9be9028d9155603bbc
Dean Radcliffe <dradcliffe@dradcliffe-laptop.(none)>
2009-01-28 00:46:56 -0600

    other half of mimetype pgn disabling

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -16,7 +16,7 @@ class MatchController < ApplicationController
     respond_to do |format|
       format.html { render :template => 'match/result' and return if @match.active == 0 }
       format.text { render :text => @match.board.to_s(@viewed_from_side==:black) }
-      format.pgn  { render :partial => 'match/move_list' }
+      # format.pgn  { render :partial => 'match/move_list' }
     end
   end
 
