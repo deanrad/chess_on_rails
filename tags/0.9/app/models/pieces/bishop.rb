d273ab916ea0dbab2d428508f50e51f2214ec0a7
Dean Radcliffe <dradcliffe@dradcliffe-laptop.(none)>
2009-03-13 04:03:32 -0500

    pruning old svn files from master branch only (svn_import should still have them)

--- tags/0.9/app/models/pieces/bishop.rb
+++
@@ -1,7 +0,0 @@
-class Bishop < Piece
-  def initialize(side, which)
-    super(:bishop, side, which)
-    
-    DIAGONAL_MOTIONS.each{ |move| @lines_of_attack << LineOfAttack.new(move) }
-  end
-end
--- tags/0.9/app/models/pieces/rook.rb
+++
@@ -1,8 +0,0 @@
-class Rook < Piece
-
-  def initialize(side, which)
-    super(:rook, side, which)
-    STRAIGHT_MOTIONS.each{ |move| @lines_of_attack << LineOfAttack.new(move) }
-  end
-  
-end
