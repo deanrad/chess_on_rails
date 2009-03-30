52f6cfbdc1fbe26e1bdf9a83ceba097b228712e1
Dean Radcliffe <dradcliffe@dradcliffe-laptop.(none)>
2009-03-30 16:20:10 -0500

    Changes to support improved authentication (part 1)

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Default routes - facebook or not
-  map.facebook_root '', :controller => "fbuser", :conditions => {:canvas => true}
+  map.facebook_root '', :controller => "match", :conditions => {:canvas => true}
   map.root :controller => "authentication", :conditions => {:canvas => false}
+
+  map.auth  'authentication/:action', :controller => 'authentication'
   map.login   'authentication/login', :controller => 'authentication', :action => 'login'
-  map.logout  'authentication/logout', :controller => 'authentication', :action => 'logout'
 
   #allow moving from CURL - Although GET generally not acceptable, post won't work without the forgery protection
   map.create_move 'match/:match_id/moves/:notation', :controller => 'move', :action => 'create', :defaults => { :notation => nil }
--- db/migrate/002_create_users.rb
+++ db/migrate/002_create_users.rb
@@ -4,7 +4,9 @@ class CreateUsers < ActiveRecord::Migration
       t.column :email, :string, :limit => 50
       t.column :playing_as, :int
       t.column :security_phrase , :string, :limit => 200
-      t.column :security_phrase_hint , :string, :limit => 200
+
+      #repurposed unused col security_phrase_hint as auth_token
+      t.column :auth_token , :string, :limit => 200
 
       t.timestamps
     end
