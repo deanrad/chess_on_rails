66b2087a7eb0681509ccfa88a875ba96addece7c
Dean Radcliffe <dradcliffe@dradcliffe-laptop.(none)>
2009-04-24 02:01:46 -0500

    protection from false-posting and gsub! errors

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,5 @@
 class ChatsController < ApplicationController
+  before_filter :authorize
 
   def show
   end
@@ -6,7 +7,10 @@ class ChatsController < ApplicationController
   # POST /match/N/chat
   def create
     chat = Chat.new(params[:chat])
-    chat.text.gsub!( '<', '&lt;' ).gsub!( '>', '&gt;' )
+    chat.text.gsub!( '<', '&lt;' ).gsub!( '>', '&gt;' ) rescue nil
+    chat.match_id = params[:match_id]
+    chat.player_id = current_player.id
+
     respond_to do |format|
       if chat.save
         format.html { render :action => :show }
