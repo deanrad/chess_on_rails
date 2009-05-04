45d73cb4f7e96b86bd14cb1f4fefa580a06525fd
Dean Radcliffe <dradcliffe@dradcliffe-laptop.(none)>
2009-05-04 09:20:32 -0500

    use of helper module to slim controller

--- app/helpers/match_helper.rb
+++ app/helpers/match_helper.rb
@@ -1,5 +1,35 @@
 module MatchHelper
 
+  def board
+    @board ||= match.board
+  end
+
+  def viewed_from_side
+    @viewed_from_side ||= (current_player == match.player1) ? :white : :black
+  end
+
+  def your_turn
+    @your_turn ||= match.turn_of?( current_player )
+  end
+
+  def last_move
+    @last_move ||= match.moves.last
+  end
+
+  def status_has_changed
+    @status_has_changed ||= ( params[:move].to_i <= match.moves.length)
+  end
+
+  # the files, in order from the viewed_from_side for rendering
+  def files
+    @files ||= (viewed_from_side == :black) ? Chess::Files.reverse : Chess::Files
+  end
+
+  # the ranks, in order from the viewed_from_side for rendering
+  def ranks
+    @ranks ||= (viewed_from_side == :black) ? Chess::Ranks : Chess::Ranks.reverse
+  end
+
   # checks for existance of .gif file in the current set's directory
   # if no .gif, uses .png extension
   def image_source_of( piece )
