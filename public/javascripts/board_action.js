a68fade91e6338edaf2f0826406c919182ee38e0
Dean Radcliffe <dradcliffe@dradcliffe-laptop.(none)>
2009-05-04 09:21:44 -0500

    getting much smarter about markup and Ajax and JSON :)

--- public/javascripts/board_action.js
+++ public/javascripts/board_action.js
@@ -18,3 +18,10 @@ function handle_release_of_piece(draggable, droparea){
    
 }
 
+function markup_piece( position, piece ){
+    //TODO render template from piece['side'], piece['function'] 
+    //if (piece == null) return;
+
+    var s = '<img alt="r" class="piece b8 c8 d8" id="q_rook_b" src="/images/sets/default/' + piece['function'] + '_' + piece['side'].subString(0,1) + '.png" />';
+    return s;
+}
