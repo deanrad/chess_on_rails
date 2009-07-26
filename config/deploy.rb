6febadff474dd376a8d4ac3ef91cf3a8624b3421
dean <dean@splash.local>
2009-07-26 14:07:11 -0500

    Set up Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+set :application, "Chess On Rails"
+
+#############################################################
+#	SCM (Git)
+#############################################################
+
+default_run_options[:pty] = true
+set :repository,  "git@github.com:chicagogrooves/chess_on_rails.git"
+set :scm, "git"
+set :branch, "master"
+ssh_options[:forward_agent] = true
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/chicagogrooves/facebook_root/"
+
+#############################################################
+#	Servers
+#############################################################
+ 
+#production
+set :domain, 'chicagogrooves.com'
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
+set :user, 'chicagogrooves'
+
+#############################################################
+#	Passenger
+#############################################################
+ 
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
