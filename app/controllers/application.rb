9b6a3cd33928946410e5e6311be1ea2e638423c2
Dean Radcliffe <dradcliffe@dradcliffe-laptop.(none)>
2009-08-05 12:18:40 -0500

    disable js for downlevel

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -38,7 +38,12 @@ class ApplicationController < ActionController::Base
       User.find_by_email_and_security_phrase(username, password).playing_as
     end
   end
-  
+
+  def downlevel?
+    @downlevel ||= request.user_agent.downcase.include? 'berry'
+  end
+  helper_method :'downlevel?'
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery :secret => '81ef9321d36cc23a2671126d90eed60f'
