dc48ccb5350fc99524cf12080dac263a04a1d722
Dean Radcliffe <dradcliffe@dradcliffe-laptop.dev.cashnetusa.com>
2009-10-29 22:06:09 -0500

    beginning of plugin for conversation (chat, actions, proposals, etc..)

---
+++ vendor/plugins/chess_on_rails_conversation/init.rb
@@ -0,0 +1,4 @@
+# Load chat model and controller.
+# We still the require the migration to live in the controller, oh well..
+#require File.expand_path(File.dirname(__FILE__) + '/app/models/chat')
+#require File.expand_path(File.dirname(__FILE__) + '/app/controllers/chats_controller')
