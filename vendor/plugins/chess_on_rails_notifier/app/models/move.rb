1dde87d44a8fb033f4794d7f49b849df7cd1add5
Dean Radcliffe <dradcliffe@dradcliffe-laptop.dev.cashnetusa.com>
2009-11-02 16:35:31 -0600

    corrected who should get the email notification of a move, put in a 1 hour minimum interval

--- vendor/plugins/chess_on_rails_notifier/app/models/move.rb
+++ vendor/plugins/chess_on_rails_notifier/app/models/move.rb
@@ -3,11 +3,14 @@ Move.class_eval do
   after_save :notify_of_move_via_email
 
   def notify_of_move_via_email
+    # TODO move to configuration - dont send email if its been less than 1/24 of a day
+    return unless(self.created_at - match.moves[ match.moves.index(self) - 1 ].created_at > 1.0 /24 )
+
     # logger.warn "Notifying by email of move #{self.inspect}"
     mover = match.gameplays[self.side].player
     opponent = match.gameplays[self.side.opposite].player
-    ChessNotifier.deliver_opponent_moved(mover, opponent, self)
-    rescue => ex
-    logger.error "Mail is fed up!  #{ex.message}"
+    ChessNotifier.deliver_opponent_moved(opponent, mover, self)
+    #rescue => ex
+    #logger.error "Mail is fed up!  #{ex.message}"
   end
 end
