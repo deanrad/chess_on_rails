a96e01f7d92f5949314911a4c0e7f8e270755b14
dean <dean@splash.local>
2009-11-01 22:41:20 -0600

    fixed bug in move notification by email

--- vendor/plugins/chess_on_rails_notifier/app/models/move.rb
+++ vendor/plugins/chess_on_rails_notifier/app/models/move.rb
@@ -3,11 +3,11 @@ Move.class_eval do
   after_save :notify_of_move_via_email
 
   def notify_of_move_via_email
-    logger.warn "Notifying by email of move #{self.inspect}"
-    mover = match.gameplays.send(self.side).player
-    opponent = match.gameplays.send(self.side.opposite).player
-    ChessNotifier.deliver_opponent_moved(plyr, opp, self)
-    rescue
-    logger.error "Mail is fed up!"
+    # logger.warn "Notifying by email of move #{self.inspect}"
+    mover = match.gameplays[self.side].player
+    opponent = match.gameplays[self.side.opposite].player
+    ChessNotifier.deliver_opponent_moved(mover, opponent, self)
+    rescue => ex
+    logger.error "Mail is fed up!  #{ex.message}"
   end
 end
