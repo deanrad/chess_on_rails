421146920f85153e9dae0e7ef28c82f67fd5e697
Dean Radcliffe <dradcliffe@dradcliffe-laptop.dev.cashnetusa.com>
2009-12-26 11:54:25 -0600

    Basic magellan functionality working (evaling, exception handling)

--- vendor/plugins/magellan_explorer/app/controllers/magellan_controller.rb
+++ vendor/plugins/magellan_explorer/app/controllers/magellan_controller.rb
@@ -24,13 +24,36 @@ class MagellanController < ApplicationController
 
     # We are trying to indicate the requests' parameters
     %w{action id controller}.each{|p| @options.delete(p) }
+
+    @original_eval_text = @options['eval_text'].dup
+    @options['safe_eval_text']  = sanitize_eval_text(@original_eval_text)
+
     true
+  end
+
+  # Attempts to prevent unsafe commands from being evaled
+  # TODO Add sanitization routines to this function as you go
+  def sanitize_eval_text(text)
+    text
+  end
+
+  # Does the actual evaluation, error handling, etc..
+  def do_eval!
+    safe_eval_text = @options['safe_eval_text']
+    if safe_eval_text
+      begin
+        @eval_output = eval(safe_eval_text).pretty_inspect
+      rescue Exception => ex
+        @eval_output = ex.message + ex.to_s
+      end
+    end
   end
 
   # Keeps you from needing a route for controller/action with no ID
   def route_id_to_action
     case params[:id]
       when "console"
+      do_eval!
       render :action => 'console'
     end
   end
