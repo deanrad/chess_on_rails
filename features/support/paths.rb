a2ff4738258c0b8f6828ab604737721be90d5a35
Dean Radcliffe <dradcliffe@dradcliffe-laptop.dev.cashnetusa.com>
2010-01-19 00:33:19 -0600

    Cucumber changes for 0.6.2, hopefully to pass RunCodeRun suite now ??

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,22 +1,27 @@
 module NavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in web_steps.rb
+  #
   def path_to(page_name)
     case page_name
     
-    when /the homepage/i
-      root_path
-    when /the sign up page/i
-      new_user_path
-    when /the sign in page/i
-      new_session_path
-    when /the password reset request page/i
-      new_password_path
+    when /the home\s?page/
+      '/'
     
-    # Add more page name => path mappings here
-    
+    # Add more mappings here.
+    # Here is an example that pulls values out of the Regexp:
+    #
+    #   when /^(.*)'s profile page$/i
+    #     user_profile_path(User.find_by_login($1))
+
     else
-      raise "Can't find mapping from \"#{page_name}\" to a path."
+      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+        "Now, go and add a mapping in #{__FILE__}"
     end
   end
 end
- 
+
 World(NavigationHelpers)
