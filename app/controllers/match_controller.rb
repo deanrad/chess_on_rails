ea96e0e92a36413aa22c09f7a775e615a07c98e7
Dean Radcliffe <chicagogrooves@gmail.com>
2010-07-19 10:14:58 -0500

    Added *extremely basic* mobile page that is capable of making a move.

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -10,6 +10,7 @@ class MatchController < ApplicationController
       format.html { render :template => 'match/result' and return if match.active == 0 }
       format.text { render :text => match.board.to_s(viewed_from_side==:black) }
       format.pgn  { render :partial => 'match/move_list' }
+      format.wml  { render :tempate => 'match/show' }
     end
   end
 
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,4 @@ Mime::Type.register "text/pgn", :pgn
 Mime::Type.register "text/fen", :fen
 
 Mime::Type.register_alias "text/html", :fbml
+Mime::Type.register_alias "text/html", :wml
