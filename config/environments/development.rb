51e800328833d36e08816d5b99215ce20d94cd8b
Dean Radcliffe <chicagogrooves@gmail.com>
2010-08-09 19:10:04 -0500

    Better dev mode mail settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,9 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
+# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :sendmail
 
 config.gem 'rails-footnotes'
 
