62395497afc4486103da54198853f15030cdce3d
Dean Radcliffe <chicagogrooves@gmail.com>
2010-08-27 08:27:08 -0500

    Part II

--- app/views/match/viewmodel.js
+++ app/views/match/viewmodel.js
@@ -1,8 +1,8 @@
 // Because client state will change while the page is loaded, we set up an object
 // with fields to track it, and methods to manipulate it. This is called a viewmodel.
 // Then we bind (using HTML5 data-bind attributes and knockout syntax) DOM elements
-// to the viewmodel attributes they are dependent on, and updating happens 
-// automatically as the viewmodel is changed, whether by user-interaction, or AJAX polls.
+// to the viewmodel, and updating happens automatically as the viewmodel is changed
+// whether by user-interaction, or AJAX polls.
 var game_view_model = {
   your_turn:                new ko.observable(<%= your_turn %>)
 };
